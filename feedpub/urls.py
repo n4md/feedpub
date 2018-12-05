1aa3e58276e0179c85d6fdda525677d5d85f6dd4
Germain Perez <germain.perez@gmail.com>
2018-12-05 06:23:17 -0500

    Minus path, plus url
    Removed imports of django.urls "path" and instead imported django.conf.urls "url" only. Changed patterns accordingly.

--- feedpub/urls.py
+++ feedpub/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, url, path
+from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
 
@@ -12,11 +12,11 @@ app_name = 'feedpub'
 #]
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('', include(admin.site.urls)), ##from ORIGINAL above
-    path('feed/<int:feed_id>/alexa/', views.alexafile, name='alexafile'),
-    path('feed/<int:feed_id>/google/', views.googlefile, name='googlefile'),
-    path('feed/<int:feed_id>/', views.feed, name='feed'),
-    path('feeditem/<int:feeditem_id>/', views.feeditem, name='feeditem'),
-    path('newitem/', views.newitem, name='newitem')
+    url('', views.index, name='index'),
+    url('', include(admin.site.urls)), ##from ORIGINAL above
+    url('feed/<int:feed_id>/alexa/', views.alexafile, name='alexafile'),
+    url('feed/<int:feed_id>/google/', views.googlefile, name='googlefile'),
+    url('feed/<int:feed_id>/', views.feed, name='feed'),
+    url('feeditem/<int:feeditem_id>/', views.feeditem, name='feeditem'),
+    url('newitem/', views.newitem, name='newitem')
 ]
